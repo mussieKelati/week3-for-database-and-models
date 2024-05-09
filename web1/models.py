fd0eff863fd6f1bed9c63bd0afdfd8e9da037038
mussieKelati <mussiekelati24@gmail.com>
2024-05-10 02:39:39 +0300

    final commit

--- web1/models.py
+++ web1/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-class student (models.Model):
+class Learner (models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     age = models.IntegerField()
--- web1/views.py
+++ web1/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from . import models
+from .models import Learner
 
 def list_students(request):
-    all_students = models.student.objects.all()
-    context = {'students': all_students}
-    return render(request, 'web1/list.html', context=context)
+    all_students = models.Learner.objects.all()
+    context = {"students": all_students}
+    return render(request, "web1/list.html",context=context)
+
+
+
